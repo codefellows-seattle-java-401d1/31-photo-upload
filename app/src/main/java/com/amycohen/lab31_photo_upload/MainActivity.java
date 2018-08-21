95ba7dde5d841f6e6ad490eab998e5b1ddda9258
Amy Cohen <anbcohen@gmail.com>
2018-08-21 12:02:19 -0700

    readded takePicture method and wired it to button via butterknife

--- app/src/main/java/com/amycohen/lab31_photo_upload/MainActivity.java
+++ app/src/main/java/com/amycohen/lab31_photo_upload/MainActivity.java
@@ -1,13 +1,19 @@
 package com.amycohen.lab31_photo_upload;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
     private StorageReference mStorageRef;
 
     @Override
@@ -15,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
         mStorageRef = FirebaseStorage.getInstance().getReference();
+    }
+
+    @OnClick(R.id.take_picture)
+    public void takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }

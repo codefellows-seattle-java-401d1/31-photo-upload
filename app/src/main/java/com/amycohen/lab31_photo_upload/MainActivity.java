6e0d65a8d6bc1026df7d01a79428e581e9c50e24
Amy Cohen <anbcohen@gmail.com>
2018-08-21 12:03:33 -0700

    added onActivityResult

--- app/src/main/java/com/amycohen/lab31_photo_upload/MainActivity.java
+++ app/src/main/java/com/amycohen/lab31_photo_upload/MainActivity.java
@@ -1,6 +1,7 @@
 package com.amycohen.lab31_photo_upload;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_SAVE_PHOTO = 2;
     private StorageReference mStorageRef;
 
     @Override
@@ -30,6 +32,24 @@ public class MainActivity extends AppCompatActivity {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            setPictureFromThumbnail(data);
+        } else if (requestCode == REQUEST_SAVE_PHOTO && resultCode == RESULT_OK) {
+            setPictureFromFile();
+        }
+    }
+
+    public void setPictureFromThumbnail (Intent data) {
+        Bundle extras = data.getExtras();
+        if (extras != null) {
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
         }
     }
 }

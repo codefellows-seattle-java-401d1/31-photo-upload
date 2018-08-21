4191519fc53296963d8e57ade58a54a51a7ec469
brandon-fenty <brandonfenty@gmail.com>
2018-08-21 13:04:11 -0700

    initial program functional, takes bitmap photos

---
+++ app/src/main/java/droid/yutani/com/a31_photo_upload/MainActivity.java
@@ -0,0 +1,46 @@
+package droid.yutani.com.a31_photo_upload;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @BindView(R.id.image_view) ImageView mImageView;
+    @BindView(R.id.thumbnail_button) Button mTakeThumbnail;
+    @BindView(R.id.picture_button) Button mTakePicture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.thumbnail_button)
+    public void takePictureIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(bitmap);
+        }
+    }
+}

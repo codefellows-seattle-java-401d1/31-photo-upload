039226e92c41ec1c09ee9618d16be56ace135062
Amy Cohen <anbcohen@gmail.com>
2018-08-21 11:58:03 -0700

    reconnected to firebase without an issue

--- app/src/main/java/com/amycohen/lab31_photo_upload/MainActivity.java
+++ app/src/main/java/com/amycohen/lab31_photo_upload/MainActivity.java
@@ -3,11 +3,18 @@ package com.amycohen.lab31_photo_upload;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class MainActivity extends AppCompatActivity {
+
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
     }
 }

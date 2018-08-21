ed8383ed0f449402374072449f0006d4724ef310
Amy Cohen <anbcohen@gmail.com>
2018-08-21 12:06:00 -0700

    added setPictureFromFile

--- app/src/main/java/com/amycohen/lab31_photo_upload/MainActivity.java
+++ app/src/main/java/com/amycohen/lab31_photo_upload/MainActivity.java
@@ -2,6 +2,7 @@ package com.amycohen.lab31_photo_upload;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -99,5 +100,39 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(takePictureIntent, REQUEST_SAVE_PHOTO);
             }
         }
+    }
+
+    private void setPictureFromFile() {
+        // Get the dimensions of the View
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        if (targetW == 0) {
+            targetW = 1;
+        }
+
+        if (targetH == 0) {
+            targetH = 1;
+        }
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        mImageView.setImageBitmap(bitmap);
+
+        uploadFile(bitmap);
     }
 }

61d6d7d448a5dfafc81c26f7488e730a4b94bd5a
brandon-fenty <brandonfenty@gmail.com>
2018-08-21 13:47:01 -0700

    converted to a single button that takes full size images, crashing after photo is taken

--- app/src/main/java/droid/yutani/com/a31_photo_upload/MainActivity.java
+++ app/src/main/java/droid/yutani/com/a31_photo_upload/MainActivity.java
@@ -2,11 +2,19 @@ package droid.yutani.com.a31_photo_upload;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,12 +22,10 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_IMAGE_CAPTURE = 1;
-    private static final int REQUEST_FULL_IMAGE_CAPTURE = 2;
 
-    private int pictureType;
+    private String mCurrentPhotoPath;
 
     @BindView(R.id.image_view) ImageView mImageView;
-    @BindView(R.id.thumbnail_button) Button mTakeThumbnail;
     @BindView(R.id.picture_button) Button mTakePicture;
 
     @Override
@@ -30,33 +36,52 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.thumbnail_button)
-    public void setThumbImg() {
-        pictureType = REQUEST_IMAGE_CAPTURE;
-        takePictureIntent();
-    }
-
     @OnClick(R.id.picture_button)
-    public void setFullImg() {
-        pictureType = REQUEST_FULL_IMAGE_CAPTURE;
-        takePictureIntent();
-    }
-
     public void takePictureIntent() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, pictureType);
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = createImageFile();
+
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(
+                        this,
+                        "droid.yutani.com.a31_photo_upload",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap bitmap = (Bitmap) extras.get("data");
             mImageView.setImageBitmap(bitmap);
-        } else if (requestCode == REQUEST_FULL_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-
         }
+    }
+
+    private File createImageFile() {
+        try {
+            // Create an image file name
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String imageFileName = "JPEG_" + timeStamp;
+            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            File image = File.createTempFile(
+                    imageFileName,
+                    ".jpg",
+                    storageDir
+            );
+            // Save a file: path for use with ACTION_VIEW intents
+            mCurrentPhotoPath = image.getAbsolutePath();
+            return image;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
